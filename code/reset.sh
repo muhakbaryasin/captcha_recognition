bf5e49394d8cff758be305234faaed1045a89e87
muhakbaryasin <m.akbar.yasin@gmail.com>
2018-05-15 10:32:29 +0700

    add

---
+++ code/reset.sh
@@ -0,0 +1,5 @@
+rm -rf /home/${USER}/Project/captcha_recognition/code/results/*
+rm -rf /home/${USER}/Project/captcha_recognition/code/temp/*
+touch /home/${USER}/Project/captcha_recognition/code/temp/readme.txt
+echo "In this folder the program will store temporary files." > /home/${USER}/Project/captcha_recognition/code/temp/readme.txt
+rm -rf /home/${USER}/Project/captcha_recognition/code/caffe_new/*
